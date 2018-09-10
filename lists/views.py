6c753df6b25a3fe9235a82a2ca7c728e11980792
Moroi Kohei <koheimoroi@yahoo.co.jp>
2018-09-10 23:09:14 +0900

    adjust model so that items are associtaed with difference lists

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -8,6 +8,11 @@ def home_page(request):
 		Item.objects.create(text=request.POST['item_text'])
 		return redirect('/lists/the-only-list-in-the-world/')
 	return render(request, 'home.html')
+
+def new_list(request):
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
 	items = Item.objects.all()
